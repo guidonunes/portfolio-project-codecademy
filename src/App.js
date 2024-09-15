9fbf94f6121927341c0a7b8e6bb39bf89f128fb3
Guilherme Nunes <guilherme.augd@gmail.com>
2024-09-15 20:16:07 -0300

    add code to the component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Journal from './features/journal/Journal';
+import BackgroundImage from './features/backgroundImage/BackgroundImage';
 
 
 function App() {
   return (
   <div className="App">
+    <BackgroundImage />
     <main>
       <Journal />
     </main>
---
+++ src/features/backgroundImage/BackgroundImage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getBackgroundImage } from './backgroundImageSlice';
+
+const BackgroundImage = () => {
+  const {imageUrls, currentImageUrlIndex} = useSelector((state) => state.backgroundImage);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getBackgroundImage());
+  }, [dispatch])
+
+  return (
+    <div id="background-image-container">
+      <img src={imageUrls[currentImageUrlIndex]} alt="background-image" />
+    </div>
+  );
+}
+
+export default BackgroundImage;
