b3e15d9aa5cdc9153def6b8a95358fa48535e005
Guilherme Nunes <guilherme.augd@gmail.com>
2024-09-15 19:54:46 -0300

    add reducers to component

--- src/features/backgroundImage/backGroundImageSlice.js
+++ src/features/backgroundImage/backGroundImageSlice.js
@@ -6,3 +6,24 @@ export const getBackgroundImage = createAsyncThunk(
   "backgroundImage/getImage",
   unsplashApi.getImage
 );
+
+
+export const backgroundImageSlice = createSlice({
+  name: "backgroundImage",
+  initialState: {
+    imageUrls: [],
+    currentImageUrlIndex:0,
+  },
+  reducers: {
+    switchToNextBackgroundImage: (state) => {
+      state.currentImageUrlIndex =
+        (state.currentImageUrlIndex + 1) % state.imageUrls.length;
+    },
+    switchToPreviousBackgroundImage: (state) => {
+      state.currentImageUrlIndex =
+        (state.currentImageUrlIndex - 1 + state.imageUrls.length) %
+        state.imageUrls.length;
+    },
+  }
+
+});
