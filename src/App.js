7001c60e2fccfabb78c1e8da15ce60e0a2bfc682
Guilherme Nunes <guilherme.augd@gmail.com>
2024-09-16 10:18:02 -0300

    fix switch button resposivenness

--- src/App.js
+++ src/App.js
@@ -4,24 +4,21 @@ import BackgroundImage from './features/backgroundImage/BackgroundImage';
 import BackgroundImageLeftControl from './features/backgroundImage/components/BackgroundImageLeftControl';
 import BackgroundImageRightControl from './features/backgroundImage/components/BackgroundImageRightControl';
 
-
-
 function App() {
   return (
-  <div className="App">
-    <BackgroundImage />
-    <aside className="left-wallpaper-control wallpaper-control">
-      <BackgroundImageLeftControl />
-    </aside>
-    <main>
-      <Journal />
-    </main>
-    <aside className="right-wallpaper-control wallpaper-control">
-      <BackgroundImageRightControl />
-    </aside>
-  </div>
+    <div className="App">
+      <BackgroundImage />
+        <aside className="left-wallpaper-control wallpaper-control">
+          <BackgroundImageLeftControl />
+        </aside>
+        <main>
+          <Journal />
+        </main>
+        <aside className="right-wallpaper-control wallpaper-control">
+          <BackgroundImageRightControl />
+        </aside>
+    </div>
   );
 }
-
 
 export default App;
