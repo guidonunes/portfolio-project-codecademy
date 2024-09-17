dbbc2fd072af7270dd4c180857ad101a56171ea4
Guilherme Nunes <guilherme.augd@gmail.com>
2024-09-17 09:57:18 -0300

    fix fetch quote api

--- src/api/quote/index.js
+++ src/api/quote/index.js
@@ -1,11 +1,22 @@
 const API_URL_QUOTE = `https://api.api-ninjas.com/v1/quotes?category=inspirational`;
+const API_KEY = process.env.REACT_APP_QUOTE_API;
 
 console.log(API_URL_QUOTE);
 
+
+
 export const getQuote = async () => {
   console.log("Fetching quote from API...");
-  const response = await fetch(API_URL_QUOTE);
-  console.log("Response:", response);
+  const response = await fetch(API_URL_QUOTE, {
+    headers: {
+      "X-Api-Key": API_KEY
+    }
+  });
+
+  if (!response.ok) {
+    console.error("Failed to fetch quote");
+    throw new Error("Failed to fetch quote");
+  }
   const json = await response.json();
   const quoteData = json[0];
   console.log("Fetched data:", quoteData);
--- src/features/quote/quoteSlice.js
+++ src/features/quote/quoteSlice.js
@@ -1,8 +1,11 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import quoteApi from '../../api/quote';
 
-
-export const getQuote = createAsyncThunk("quote/getQuote", quoteApi.getQuote);
+// Async thunk to fetch the quote
+export const getQuote = createAsyncThunk("quote/getQuote", async () => {
+  const response = await quoteApi.getQuote();
+  return response[0]; // Extract the first object from the array
+});
 
 const quoteSlice = createSlice({
   name: "quote",
@@ -10,11 +13,14 @@ const quoteSlice = createSlice({
     quote: "",
     author: "",
   },
-  reducers: (builder) => ({}),
+  reducers: {},
   extraReducers: (builder) => {
     builder.addCase(getQuote.fulfilled, (state, action) => {
       state.quote = action.payload.quote;
       state.author = action.payload.author;
+    });
+    builder.addCase(getQuote.rejected, (state, action) => {
+      console.error('Failed to fetch quote:', action.error.message);
     });
   },
 });
