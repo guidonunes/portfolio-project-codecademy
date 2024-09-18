7670b392a4996f6411ea6cf4ffe69265b4a19644
Guilherme Nunes <guilherme.augd@gmail.com>
2024-09-18 09:10:42 -0300

    fix typo

--- src/api/openWeather/index.js
+++ src/api/openWeather/index.js
@@ -1,5 +1,3 @@
-import { get } from "http";
-
 const API_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.REACT_APP_OPEN_WEATHER_KEY}&units=metric`;
 
 console.log('Open Weather API Key:', process.env.REACT_APP_OPEN_WEATHER_KEY);
